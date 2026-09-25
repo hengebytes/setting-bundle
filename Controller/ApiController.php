c0492e4e833c8602b6a64a97d244ebdb0bce8c08
Aat <at@conciliolabs.com>
2026-09-25 03:12:05 +0300

    fix deprecation

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Exception\JsonException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/settings', 'hengebytes_settings_api_')]
 readonly class ApiController
